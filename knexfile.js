1b279548c4166957cbe85ba345f3ebb01974f594
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-23 23:53:28 +0000

    Init migration for dev

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+const settings = require('./settings');
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: settings,
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
